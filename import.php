6a10f9cf11651a8163f4746ac5f9e56e8ef3a898
Josh DeSherlia <josh@desherlia.net>
2016-01-28 11:37:46 -0800

    Added blueprints and adjusted import.php to run during onPageInitialized

--- import.php
+++ import.php
@@ -1,28 +1,23 @@
 <?php
 	namespace Grav\Plugin;
 
-	// use Grav\Common\Page\Collection;
 	use Grav\Common\Plugin;
 	use Symfony\Component\Yaml\Yaml;
-	// use Grav\Common\Uri;
-	// use Grav\Common\Taxonomy;
 
 	class ImportPlugin extends Plugin
 	{
 		public static function getSubscribedEvents() {
 		    return [
-		        'onPageContentRaw' => ['onPageContentRaw', 0],
+		        'onPageInitialized' => ['onPageInitialized', 0],
 		    ];
 		}
 
-		public function onPageContentRaw()
+		public function onPageInitialized()
 		{
-			$page = $this->grav['page'];
+			if (property_exists($this->grav['page']->header(),'imports')) {
 
-			if (property_exists($page->header(),'imports')) {
-
-				$path = $page->path();
-				$imports = $page->header()->imports;
+				$path = $this->grav['page']->path();
+				$imports = $this->grav['page']->header()->imports;
                 $parsed = [];
 				
 				if (is_array($imports)) {
@@ -35,7 +30,7 @@
 					$parsed = Yaml::parse(file_get_contents($path.'/'.$imports));
 				}
 
-                $page->header()->imports = $parsed;
+                $this->grav['page']->header()->imports = $parsed;
 			}
 		}
 
