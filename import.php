e416103c176da317facea5493d504e7e346cbb2f
Aaron Dalton <aaron@daltons.ca>
2016-10-30 19:50:40 +0000

    - Added JSON functionality - Moved from hardcoded '/' to built-in DS constant - Added 'data:' prefix - Added sanitize function for path names - Overhauled README

--- import.php
+++ import.php
@@ -1,37 +1,70 @@
 <?php
-	namespace Grav\Plugin;
+    namespace Grav\Plugin;
 
-	use Grav\Common\Plugin;
-	use Symfony\Component\Yaml\Yaml;
+    use Grav\Common\Plugin;
+    use Grav\Common\Utils;
+    use Symfony\Component\Yaml\Yaml;
 
-	class ImportPlugin extends Plugin
-	{
-		public static function getSubscribedEvents() {
-		    return [
-		        'onPageInitialized' => ['onPageInitialized', 0],
-		    ];
-		}
+    class ImportPlugin extends Plugin
+    {
+        public static function getSubscribedEvents() {
+            return [
+                'onPageInitialized' => ['onPageInitialized', 0],
+            ];
+        }
 
-		public function onPageInitialized()
-		{
-			if (property_exists($this->grav['page']->header(),'imports')) {
+        public function onPageInitialized()
+        {
+            if (property_exists($this->grav['page']->header(),'imports')) {
 
-				$path = $this->grav['page']->path();
-				$imports = $this->grav['page']->header()->imports;
+                $imports = $this->grav['page']->header()->imports;
                 $parsed = [];
-				
-				if (is_array($imports)) {
 
-					foreach ($imports as $import) {
-                        $yamlName = rtrim($import,'.yaml');
-                        $parsed[$yamlName] = Yaml::parse(file_get_contents($path.'/'.$import));
-					}
-				} else {
-					$parsed = Yaml::parse(file_get_contents($path.'/'.$imports));
-				}
+                if (is_array($imports)) {
+                    foreach ($imports as $import) {
+                        $import = static::sanitize($import);
+                        $key = ltrim($import, 'data:');
+                        if (Utils::endswith($import, '.yaml')) {
+                            $key = rtrim($key,'.yaml');
+                            $parsed[$key] = Yaml::parse($this->getContents($import));
+                        } elseif (Utils::endswith($import, '.json')) {
+                            $key = rtrim($key,'.json');
+                            $parsed[$key] = json_decode($this->getContents($import));
+                        }
+                    }
+                } else {
+                    $import = static::sanitize($import);
+                    if (Utils::endswith($import, '.yaml')) {
+                        $parsed = Yaml::parse($this->getContents($import));
+                    } elseif (Utils::endswith($import, '.json')) {
+                        $parsed = json_decode($this->getContents($import));
+                    }
+                }
 
                 $this->grav['page']->header()->imports = $parsed;
-			}
-		}
+            }
+        }
 
-	}
+        private function getContents($fn) {
+            if (Utils::startswith($fn, 'data:')) {
+                $path = $this->grav['locator']->findResource('user://data', true);
+                $fn = ltrim($fn, 'data:');
+            } else {
+                $path = $this->grav['page']->path();
+            }
+            $path = $path . DS . $fn;
+            if (file_exists($path)) {
+                return file_get_contents($path);
+            }
+            return null;
+        }
+
+        private static function sanitize($fn) {
+            $fn = trim($fn);
+            $fn = str_replace('..', '', $fn);
+            $fn = ltrim($fn, DS);
+            $fn = str_replace(DS.DS, DS, $fn);
+            return $fn;
+        }
+
+    }
